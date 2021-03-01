4cb84c79d100559360659ae431173ab5023f7c5b
iMactool <weidj201314@live.cn>
2021-03-01 19:18:50 +0800

    Apply fixes from StyleCI (#5)
    Co-authored-by: iMactool <iMactool@users.noreply.github.com>

--- src/Http/AuthService.php
+++ src/Http/AuthService.php
@@ -1,8 +1,9 @@
 <?php
 
 /*
- * * Author: cc
- *  * Created by PhpStorm.
+ * Author: cc
+ *  Created by PhpStorm.
+ *  Copyright (c)  cc Inc. All rights reserved.
  */
 
 namespace Imactool\Jinritemai\Http;
