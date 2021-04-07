df48a4e04d6ba56ea6e781985719a4d1f2fb6dac
weslee <chinauser1208@gmail.com>
2021-04-07 10:12:45 +0800

    fix bug.

--- src/Order/Order.php
+++ src/Order/Order.php
@@ -15,84 +15,84 @@ class Order extends BaseService
     //添加订单备注 addOrderRemark()
     public function addOrderRemark(array $params)
     {
-        $this->post('order/addOrderRemark', $params);
+       return $this->post('order/addOrderRemark', $params);
     }
 
     //设置店铺设置地址变更审核 setAddressAppliedSwitch()
     public function setAddressAppliedSwitch(array $params)
     {
-        $this->post('order/AddressAppliedSwitch', $params);
+        return $this->post('order/AddressAppliedSwitch', $params);
     }
 
     //买家地址变更确认 addressConfirm()
     public function addressConfirm(array $params)
     {
-        $this->post('order/addressConfirm', $params);
+        return $this->post('order/addressConfirm', $params);
     }
 
     //买家主动修改收货地址 addressModify()
     public function addressModify(array $params)
     {
-        $this->post('order/addressModify', $params);
+        return $this->post('order/addressModify', $params);
     }
 
     //取消(货到付款)订单 cancelOrder()
     public function cancelOrder(array $params)
     {
-        $this->post('order/cancel', $params);
+        return $this->post('order/cancel', $params);
     }
 
     //获取订单详情 getOrder()
     public function getOrder(array $params)
     {
-        $this->post('order/detail', $params);
+        return $this->post('order/detail', $params);
     }
 
     //获取服务单列表 getServiceOrderList()
     public function getServiceOrderList(array $params)
     {
-        $this->post('order/getServiceList', $params);
+        return $this->post('order/getServiceList', $params);
     }
 
     //获取订单列 getOrderList()
     public function getOrderList(array $params)
     {
-        $this->post('order/list', $params);
+        return $this->post('order/list', $params);
     }
 
     //回复服务请求 replyService()
     public function replyService(array $params)
     {
-        $this->post('order/replyService', $params);
+        return $this->post('order/replyService', $params);
     }
 
     //查询商家服务单详情请求 serviceDetail()
     public function serviceDetail(array $params)
     {
-        $this->post('order/serviceDetail', $params);
+        return  $this->post('order/serviceDetail', $params);
     }
 
     //获取服务请求列表 serviceList()
     public function serviceList(array $params)
     {
-        $this->post('order/serviceList', $params);
+        return $this->post('order/serviceList', $params);
     }
 
     //确认货到付款订单 confirmOrder()
     public function confirmOrder(array $params)
     {
-        $this->post('order/stockUp', $params);
+        return $this->post('order/stockUp', $params);
     }
 
     //未支付订单改货款 updateOrderAmount()
     public function updateOrderAmount(array $params)
     {
-        $this->post('order/updateOrderAmount', $params);
+        return  $this->post('order/updateOrderAmount', $params);
     }
 
     //未支付订单邮费修改 updateOrderPostAmount()
     public function updateOrderPostAmount(array $params)
     {
-        $this->post('order/updatePostAmount', $params);
+        return $this->post('order/updatePostAmount', $params);
     }
 }
